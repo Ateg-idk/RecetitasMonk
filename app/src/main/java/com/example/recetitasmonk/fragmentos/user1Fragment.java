b1cf59a0c534dff619e979cbd85470d92808c190
gabriel lozano rosales <gabriellozanorosales@gmail.com>
2024-06-15 01:48:37 -0500

    cambio de idioma

--- app/src/main/java/com/example/recetitasmonk/fragmentos/user1Fragment.java
+++ app/src/main/java/com/example/recetitasmonk/fragmentos/user1Fragment.java
@@ -1,16 +1,23 @@
 package com.example.recetitasmonk.fragmentos;
-
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
+import android.widget.Toast;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import java.util.Locale;
+import android.os.Build;
+import android.content.res.Configuration;
 
 import com.example.recetitasmonk.R;
 import com.example.recetitasmonk.actividades.EditarActivity;
@@ -21,6 +28,8 @@ import com.example.recetitasmonk.actividades.PoliticasPrivacidadActivity;
 import com.example.recetitasmonk.actividades.TerminoCondicionesActivity;
 import com.example.recetitasmonk.sqlite.RecetitasMonk;
 
+import java.util.Locale;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link user1Fragment#newInstance} factory method to
@@ -28,7 +37,11 @@ import com.example.recetitasmonk.sqlite.RecetitasMonk;
  */
 public class user1Fragment extends Fragment implements View.OnClickListener{
 
+
+    Spinner cboIdioma;
+    Button BtnGuardar, BtnCerrarses;
     LinearLayout btnCerrarSesion;
+
     LinearLayout editarPerfil,misrecetas,misfavoritos,politicaPrivacidad,TerminoCondiciones;
 
     // TODO: Rename parameter arguments, choose names that match
@@ -82,7 +95,9 @@ public class user1Fragment extends Fragment implements View.OnClickListener{
         misfavoritos = vista.findViewById(R.id.misfavoritos);
         politicaPrivacidad = vista.findViewById(R.id.politicaPrivacidad);
         TerminoCondiciones = vista.findViewById(R.id.TerminoCondiciones);
-
+        BtnGuardar = vista.findViewById(R.id.frgCfgGuardar);
+        cboIdioma = vista.findViewById(R.id.frgCfgIdioma);
+        BtnCerrarses = vista.findViewById(R.id.frgCerrarses);
 
         btnCerrarSesion.setOnClickListener(this);
         misfavoritos.setOnClickListener(this);
@@ -90,15 +105,57 @@ public class user1Fragment extends Fragment implements View.OnClickListener{
         misrecetas.setOnClickListener(this);
         politicaPrivacidad.setOnClickListener(this);
         TerminoCondiciones.setOnClickListener(this);
+        BtnGuardar.setOnClickListener(this);
+        BtnCerrarses.setOnClickListener(this);
+        //cargamos idiomas al combobox
+        llenarIdioma();
+        cargarPreferencias();
 
 
         return vista;
     }
 
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Resources resources = getResources();
+        Configuration config = new Configuration(resources.getConfiguration());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            config.setLocale(locale);
+        } else {
+            config.locale = locale;
+        }
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+
+        // Reiniciar la actividad para aplicar los cambios
+        getActivity().recreate();
+    }
+
+    private void llenarIdioma() {
+        String[] idiomas = {"Elija un idioma", "Español", "Inglés"};
+        cboIdioma.setAdapter(new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                idiomas));
+
+    }
+
+    private void cargarPreferencias() {
+        SharedPreferences preferences = getActivity().getSharedPreferences("Preferencias", Context.MODE_PRIVATE);
+        int idioma = preferences.getInt("Idioma", 0);
+        cboIdioma.setSelection(idioma);
+    }
+
+
+
     @Override
     public void onClick(View v) {
+        
         if(v.getId() == R.id.btnCerrarSesion)
             cerrarSesion();
+        if(v.getId() == R.id.frgCfgGuardar)
+            Guardar();
+        else if(v.getId() == R.id.frgCerrarses)
+            CerrarSesion();
         else if (v.getId()==R.id.editarPerfil){
             cargarperfil();
         }else if (v.getId()==R.id.misrecetas){
@@ -111,6 +168,40 @@ public class user1Fragment extends Fragment implements View.OnClickListener{
             cargarTerminos();
         }
     }
+
+    private void CerrarSesion() {
+    }
+
+    private void Guardar() {
+        if (cboIdioma.getSelectedItemPosition() != 0) {
+            SharedPreferences preferences = getActivity().getSharedPreferences("Preferencias", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+
+
+
+            editor.putInt("Idioma", cboIdioma.getSelectedItemPosition());
+            editor.commit();
+
+            // Obtener el idioma seleccionado
+            String selectedLanguage = "";
+            switch (cboIdioma.getSelectedItemPosition()) {
+                case 1:
+                    selectedLanguage = "es"; // Español
+                    break;
+                case 2:
+                    selectedLanguage = "en"; // Inglés
+                    break;
+            }
+
+            // Cambiar el idioma
+            setLocale(selectedLanguage);
+
+            Toast.makeText(getContext(), "Preferencias Guardadas", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getContext(), "Debes elegir un idioma", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     private void cerrarSesion() {
         RecetitasMonk tm = new RecetitasMonk(getContext());
