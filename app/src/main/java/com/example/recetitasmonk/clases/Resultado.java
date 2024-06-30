b005743594dfc2190deace6f6551f5a32b8371f5
Angelo Oro Anticona <angelooroanticona13@gmail.com>
2024-06-30 09:03:40 -0500

    Si

---
+++ app/src/main/java/com/example/recetitasmonk/clases/Resultado.java
@@ -0,0 +1,88 @@
+package com.example.recetitasmonk.clases;
+import java.io.Serializable;
+public class Resultado implements Serializable {
+
+    private int idRecetas;
+    private String nombreReceta;
+    private String ingredientes;
+    private String preparacion;
+    private String imagen;
+    private String departamento;
+    private  int idUsuarios;
+    private int idCategoria;
+
+    public Resultado(int idRecetas, String nombreReceta, String ingredientes, String preparacion, String imagen, String departamento, int idUsuarios, int idCategoria) {
+        this.idRecetas = idRecetas;
+        this.nombreReceta = nombreReceta;
+        this.ingredientes = ingredientes;
+        this.preparacion = preparacion;
+        this.imagen = imagen;
+        this.departamento = departamento;
+        this.idUsuarios = idUsuarios;
+        this.idCategoria = idCategoria;
+    }
+
+    public int getIdRecetas() {
+        return idRecetas;
+    }
+
+    public void setIdRecetas(int idRecetas) {
+        this.idRecetas = idRecetas;
+    }
+
+    public String getNombreReceta() {
+        return nombreReceta;
+    }
+
+    public void setNombreReceta(String nombreReceta) {
+        this.nombreReceta = nombreReceta;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public String getPreparacion() {
+        return preparacion;
+    }
+
+    public void setPreparacion(String preparacion) {
+        this.preparacion = preparacion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public int getIdUsuarios() {
+        return idUsuarios;
+    }
+
+    public void setIdUsuarios(int idUsuarios) {
+        this.idUsuarios = idUsuarios;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+}
