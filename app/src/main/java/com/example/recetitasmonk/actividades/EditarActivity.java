898107ea564a869066a3e725bd73c1fbb3e74ca8
unknown <fabian13sosaalva@gmail.com>
2024-06-29 22:04:27 -0500

    mostrar Datos

--- app/src/main/java/com/example/recetitasmonk/actividades/EditarActivity.java
+++ app/src/main/java/com/example/recetitasmonk/actividades/EditarActivity.java
@@ -1,6 +1,11 @@
 package com.example.recetitasmonk.actividades;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +14,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.recetitasmonk.R;
+import com.example.recetitasmonk.clases.Cliente;
+import com.example.recetitasmonk.fragmentos.otroFragment;
+import com.example.recetitasmonk.fragmentos.user1Fragment;
 
-public class EditarActivity extends AppCompatActivity {
+public class EditarActivity extends AppCompatActivity implements View.OnClickListener {
+    Cliente cliente;
+    ImageView atras;
+    TextView nombreuser, apellidouser, correouser, passuser;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +31,23 @@ public class EditarActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_editar);
 
+        atras = findViewById(R.id.backButton);
+        cliente = (Cliente) getIntent().getSerializableExtra("cliente");
+
+        nombreuser = findViewById(R.id.name_editText);
+        nombreuser.setText(cliente.getNombre());
+        apellidouser = findViewById(R.id.last_name_editText);
+        apellidouser.setText(cliente.getApellidoP());
+        correouser = findViewById(R.id.email_editText);
+        correouser.setText(cliente.getCorreo());
+        passuser = findViewById(R.id.txtContraseña);
+        passuser.setText(cliente.getContraseña());
+        atras.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId()==R.id.backButton)
+        onBackPressed();
     }
 }
